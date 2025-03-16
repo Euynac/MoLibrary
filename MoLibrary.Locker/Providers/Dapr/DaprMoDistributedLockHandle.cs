1cc2e6d9dc0a7d5e09db2a04835e7d67502498a3
Momean <34342521+Tairitsua@users.noreply.github.com>
2025-03-16 20:48:01 +0800

    Add distributed locking mechanism to MoLibrary
    This commit introduces a new project `MoLibrary.Locker` targeting .NET 8.0, which includes various implementations of distributed locks using Dapr, local storage, and Medallion. Key additions include:
    
    - New attribute class `AutoControllerGeneratorConfigAttribute`.
    - Implementation of `IDistributedLockKeyNormalizer` and its concrete class `DistributedLockKeyNormalizer`.
    - Interfaces for distributed locks: `IMoDistributedLock` and `IMoDistributedLockHandle`.
    - Configuration classes: `MoDistributedLockOptions` and `MoDistributedLockDaprOptions`.
    - Classes for managing distributed locks: `DaprMoDistributedLock`, `LocalMoDistributedLock`, and `MedallionMoDistributedLock`, along with their respective handle classes.
    
    These changes provide a structured approach to managing distributed locks within the MoLibrary project.

---
+++ MoLibrary.Locker/Providers/Dapr/DaprMoDistributedLockHandle.cs
@@ -0,0 +1,14 @@
+﻿using Dapr.Client;
+using MoLibrary.Locker.DistributedLocking;
+
+namespace MoLibrary.Locker.Providers.Dapr;
+
+public class DaprMoDistributedLockHandle(TryLockResponse lockResponse) : IMoDistributedLockHandle
+{
+    protected TryLockResponse LockResponse { get; } = lockResponse;
+
+    public async ValueTask DisposeAsync()
+    {
+        await LockResponse.DisposeAsync();
+    }
+}
---
+++ MoLibrary.Locker/Providers/Dapr/MoDistributedLockDaprOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace MoLibrary.Locker.Providers.Dapr;
+
+public class MoDistributedLockDaprOptions
+{
+    public string StoreName { get; set; } = default!;
+
+    public string? Owner { get; set; }
+
+    public TimeSpan DefaultExpirationTimeout { get; set; } = TimeSpan.FromMinutes(2);
+}
