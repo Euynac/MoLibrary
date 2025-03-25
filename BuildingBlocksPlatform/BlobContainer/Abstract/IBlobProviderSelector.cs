06c0248bd4da604e93a623adc822cfdfd116da44
Euynac <34342521+Tairitsua@users.noreply.github.com>
2025-03-25 15:46:40 +0800

    移植回去

--- BuildingBlocksPlatform/BlobContainer/Abstract/IBlobProviderSelector.cs
+++
@@ -1,8 +0,0 @@
-using JetBrains.Annotations;
-
-namespace BuildingBlocksPlatform.BlobContainer.Abstract;
-
-public interface IBlobProviderSelector
-{
-    IBlobProvider Get(string containerName);
-}
