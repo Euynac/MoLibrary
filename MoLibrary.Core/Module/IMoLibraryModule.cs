d4a2e66b8422c4a8340e5de9769d96dd29f0b88d
Momean <18819458351@163.com>
2025-04-15 20:16:19 +0800

    Module扩展

--- MoLibrary.Core/Module/IMoLibraryModule.cs
+++ MoLibrary.Core/Module/IMoLibraryModule.cs
@@ -27,37 +27,3 @@ public interface IMoLibraryModule
     /// <param name="application">应用程序构建器</param>
     void UseMiddlewares(IApplicationBuilder application);
 }
-
-/// <summary>
-/// MoLibrary模块抽象基类
-/// 提供IMoLibraryModule接口的默认实现
-/// </summary>
-public abstract class MoLibraryModule : IMoLibraryModule
-{
-    /// <summary>
-    /// 配置WebApplicationBuilder
-    /// 默认实现为空，子类可根据需要重写
-    /// </summary>
-    /// <param name="builder">WebApplicationBuilder实例</param>
-    public virtual void ConfigureBuilder(WebApplicationBuilder builder)
-    {
-    }
-
-    /// <summary>
-    /// 配置服务依赖注入
-    /// 默认实现为空，子类可根据需要重写
-    /// </summary>
-    /// <param name="services">服务集合</param>
-    public virtual void ConfigureServices(IServiceCollection services)
-    {
-    }
-
-    /// <summary>
-    /// 使用中间件
-    /// 默认实现为空，子类可根据需要重写
-    /// </summary>
-    /// <param name="application">应用程序构建器</param>
-    public virtual void UseMiddlewares(IApplicationBuilder application)
-    {
-    }
-}
---
+++ MoLibrary.Core/Module/MoModule.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MoLibrary.Core.Module;
+
+public class ModuleExtraActionInfo
+{
+    public Action<IServiceCollection>? ConfigureServices { get; set; }
+    public string? Key { get; set; }
+}
+
+/// <summary>
+/// MoLibrary模块抽象基类
+/// 提供IMoLibraryModule接口的默认实现
+/// </summary>
+public abstract class MoModule : IMoLibraryModule
+{
+    public static Dictionary<Type, List<ModuleExtraActionInfo>> ModuleExtraActions { get; set; } = new();
+
+    /// <summary>
+    /// 配置WebApplicationBuilder
+    /// 默认实现为空，子类可根据需要重写
+    /// </summary>
+    /// <param name="builder">WebApplicationBuilder实例</param>
+    public virtual void ConfigureBuilder(WebApplicationBuilder builder)
+    {
+    }
+
+    /// <summary>
+    /// 配置服务依赖注入
+    /// 默认实现为空，子类可根据需要重写
+    /// </summary>
+    /// <param name="services">服务集合</param>
+    public virtual void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    /// <summary>
+    /// 使用中间件
+    /// 默认实现为空，子类可根据需要重写
+    /// </summary>
+    /// <param name="application">应用程序构建器</param>
+    public virtual void UseMiddlewares(IApplicationBuilder application)
+    {
+    }
+
+
+    public static void ConfigureExtraServices<TModule>(Action<IServiceCollection> configureServices) where TModule : MoModule
+    {
+        var moduleType = typeof(TModule);
+        if (!ModuleExtraActions.TryGetValue(moduleType, out var extraActions))
+        {
+            extraActions = new List<ModuleExtraActionInfo>();
+            ModuleExtraActions[moduleType] = extraActions;
+        }
+        extraActions.Add(new ModuleExtraActionInfo { ConfigureServices = configureServices });
+    }
+
+    public static void ConfigureExtraServicesOnce<TModule>(string key, Action<IServiceCollection> configureServices) where TModule : MoModule
+    {
+        var moduleType = typeof(TModule);
+        if (!ModuleExtraActions.TryGetValue(moduleType, out var extraActions))
+        {
+            extraActions = new List<ModuleExtraActionInfo>();
+            ModuleExtraActions[moduleType] = extraActions;
+        }
+        if (extraActions.Any(x => x.Key == key))
+        {
+            return;
+            //throw new InvalidOperationException($"ConfigureExtraServicesOnce for {moduleType} with key {key} already exists");
+        }
+        extraActions.Add(new ModuleExtraActionInfo { ConfigureServices = configureServices, Key = key });
+    }
+}
