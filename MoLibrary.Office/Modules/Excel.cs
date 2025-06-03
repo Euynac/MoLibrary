041fbdf97aeb82f3861a62ffe08058bc255e331e
Momean <18819458351@163.com>
2025-06-03 11:15:18 +0800

    模块化Excel

--- MoLibrary.Office/Modules/Excel.cs
+++ MoLibrary.Office/Modules/Excel.cs
@@ -3,9 +3,16 @@ using Microsoft.Extensions.DependencyInjection;
 using MoLibrary.Core.Module;
 using MoLibrary.Core.Module.Interfaces;
 using MoLibrary.Core.Module.Models;
+using MoLibrary.Office.Excel.Npoi.Export;
+using MoLibrary.Office.Excel.Npoi.Import;
+using MoLibrary.Office.Excel.Npoi;
+using MoLibrary.Office.Excel;
 using MoLibrary.Tool.MoResponse;
+using MoLibrary.Office.Excel.EpPlus.Export;
+using MoLibrary.Office.Excel.EpPlus.Import;
+using MoLibrary.Office.Excel.EpPlus;
 
-namespace MoLibrary.Excel.Modules;
+namespace MoLibrary.Office.Modules;
 
 
 public static class ModuleExcelBuilderExtensions
@@ -26,15 +33,53 @@ public class ModuleExcel(ModuleExcelOption option) : MoModule<ModuleExcel, Modul
 
     public override Res ConfigureServices(IServiceCollection services)
     {
-
         return Res.Ok();
     }
 }
 
+/// <summary>
+/// Excel模块配置指南
+/// </summary>
 public class ModuleExcelGuide : MoModuleGuide<ModuleExcel, ModuleExcelOption, ModuleExcelGuide>
 {
+    private const string SET_EXCEL_PROVIDER = nameof(SET_EXCEL_PROVIDER);
+    protected override string[] GetRequestedConfigMethodKeys()
+    {
+        return [SET_EXCEL_PROVIDER];
+    }
+    /// <summary>
+    /// 使用 NPOI excel导入导出
+    /// </summary>
+    /// <returns></returns>
+    public ModuleExcelGuide UseNpoiExcel()
+    {
+        ConfigureServices(SET_EXCEL_PROVIDER, context =>
+        {
+            context.Services.AddSingleton<INpoiCellStyleHandle, NpoiCellStyleHandle>();
+            context.Services.AddSingleton<INpoiExcelHandle, NpoiExcelHandle>();
 
+            context.Services.AddTransient<IExcelImportManager, NpoiExcelImportProvider>();
+            context.Services.AddTransient<IExcelExportManager, NpoiExcelExportProvider>();
+        });
+        return this;
+    }
 
+    /// <summary>
+    /// 使用 EpPlus excel导入导出
+    /// </summary>
+    /// <returns></returns>
+    public ModuleExcelGuide UseEpPlusExcel()
+    {
+        ConfigureServices(SET_EXCEL_PROVIDER, context =>
+        {
+            context.Services.AddSingleton<IEpPlusCellStyleHandle, EpPlusCellStyleHandle>();
+            context.Services.AddSingleton<IEpPlusExcelHandle, EpPlusExcelHandle>();
+
+            context.Services.AddTransient<IExcelImportManager, EpPlusExcelImportProvider>();
+            context.Services.AddTransient<IExcelExportManager, EpPlusExcelExportProvider>();
+        });
+        return this;
+    }
 }
 
 public class ModuleExcelOption : MoModuleOption<ModuleExcel>
