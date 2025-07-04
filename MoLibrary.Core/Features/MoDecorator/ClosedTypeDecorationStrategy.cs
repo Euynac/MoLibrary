0cdac1cc92b9092e4e51805a746d6967330d6826
Momean <18819458351@163.com>
2025-07-04 11:46:18 +0800

    迁移装饰器实现（Scrutor），并尝试扩展支持抽象装饰

---
+++ MoLibrary.Core/Features/MoDecorator/ClosedTypeDecorationStrategy.cs
@@ -0,0 +1,30 @@
+namespace MoLibrary.Core.Features.MoDecorator;
+
+internal sealed class ClosedTypeDecorationStrategy(
+    Type serviceType,
+    string? serviceKey,
+    Type? decoratorType,
+    Func<object, IServiceProvider, object>? decoratorFactory)
+    : DecorationStrategy(serviceType, serviceKey)
+{
+    private Type? DecoratorType { get; } = decoratorType;
+
+    private Func<object, IServiceProvider, object>? DecoratorFactory { get; } = decoratorFactory;
+
+    protected override bool CanDecorate(Type serviceType) => ServiceType == serviceType;
+
+    public override Func<IServiceProvider, object?, object> CreateDecorator(Type serviceType, string serviceKey)
+    {
+        if (DecoratorType is not null)
+        {
+            return TypeDecorator(serviceType, serviceKey, DecoratorType);
+        }
+
+        if (DecoratorFactory is not null)
+        {
+            return FactoryDecorator(serviceType, serviceKey, DecoratorFactory);
+        }
+
+        throw new InvalidOperationException($"Both {nameof(DecoratorType)} and {nameof(DecoratorFactory)} can not be null.");
+    }
+}
