b08816edd6ed7f9178467edf9bb6ee6cbaf531c6
Momean <34342521+Tairitsua@users.noreply.github.com>
2025-07-15 23:07:39 +0800

    Add SignalR endpoint configuration and Hub registration
    Updated `ModuleSignalR` to include new endpoint configuration for SignalR, allowing retrieval of registered Hub methods with OpenAPI documentation. Refactored `MapMoHub` in `ModuleSignalRGuide` to utilize the new configuration method. Introduced a new `Hubs` property in `ModuleSignalROption` to store registered Hub types.

--- MoLibrary.SignalR/Modules/ModuleSignalROption.cs
+++ MoLibrary.SignalR/Modules/ModuleSignalROption.cs
@@ -5,4 +5,9 @@ namespace MoLibrary.SignalR.Modules;
 public class ModuleSignalROption : MoModuleControllerOption<ModuleSignalR>
 {
     public string ServerMethodsRoute { get; set; } = "/signalr/hubs";
+
+    /// <summary>
+    /// 注册的Hub类型
+    /// </summary>
+    internal List<Type> Hubs { get; set; } = [];
 }
