06e1fabe0fec3dd7aaacc5194ff3dbd17abaa3b9
Momean <18819458351@163.com>
2025-07-16 13:50:56 +0800

    优化signalR分组

--- MoLibrary.SignalR/Modules/ModuleSignalROption.cs
+++ MoLibrary.SignalR/Modules/ModuleSignalROption.cs
@@ -7,5 +7,7 @@ public class ModuleSignalROption : MoModuleControllerOption<ModuleSignalR>
     /// <summary>
     /// 注册的Hub类型
     /// </summary>
-    internal List<Type> Hubs { get; set; } = [];
+    internal List<MoHubInfo> Hubs { get; set; } = [];
 }
+
+public record MoHubInfo(Type HubType, string HubRoute);
