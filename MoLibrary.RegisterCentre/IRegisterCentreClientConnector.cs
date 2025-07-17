9d5460940ca7b7cd295f73a968ea9eac0fa2f666
Momean <18819458351@163.com>
2025-07-17 14:15:15 +0800

    优化

--- MoLibrary.RegisterCentre/IRegisterCentreClientConnector.cs
+++ MoLibrary.RegisterCentre/IRegisterCentreClientConnector.cs
@@ -53,14 +53,6 @@ public class DaprHttpForConnectClient(DaprClient client, ILogger<DaprHttpForConn
                 throw new InvocationException(appid, callbackUrl,
                     new Exception("Json序列化为空"), response);
 
-            if (res is IMoResponse serviceResponse)
-            {
-                serviceResponse.AutoParseResponseFromOrigin(content);
-            }
-
-            //var res = await response.Content.ReadFromJsonAsync<TResponse>(jsonOption.GlobalOptions);
-
-
             return res;
         }
         catch (JsonException jsonException)
@@ -103,10 +95,6 @@ public class DaprHttpForConnectClient(DaprClient client, ILogger<DaprHttpForConn
                 throw new InvocationException(appid, callbackUrl,
                     new Exception("Json序列化为空"), response);
 
-            if (res is IMoResponse serviceResponse)
-            {
-                serviceResponse.AutoParseResponseFromOrigin(content);
-            }
             return res;
         }
         catch (JsonException jsonException)
