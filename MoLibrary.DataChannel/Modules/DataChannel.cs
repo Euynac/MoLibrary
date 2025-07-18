6e8cbc082ce96561e00783b012df2b9d3f3ea40e
Momean <34342521+Tairitsua@users.noreply.github.com>
2025-07-18 16:10:39 +0800

    调整DataChannel模块依赖

--- MoLibrary.DataChannel/Modules/DataChannel.cs
+++ MoLibrary.DataChannel/Modules/DataChannel.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using MoLibrary.Core.Module;
+using MoLibrary.Core.Module.Interfaces;
 using MoLibrary.Core.Module.Models;
-using MoLibrary.Core.Module.ModuleController;
 using MoLibrary.Core.Modules;
+using MoLibrary.Dapr.Modules;
 using MoLibrary.DataChannel.Dashboard.Controllers;
 using MoLibrary.DataChannel.Interfaces;
 using MoLibrary.DataChannel.Services;
@@ -58,5 +58,52 @@ public class ModuleDataChannel(ModuleDataChannelOption option)
     public override void ClaimDependencies()
     {
         DependsOnModule<ModuleControllersGuide>().Register().RegisterMoControllers<ModuleDataChannelController>(Option);
+        DependsOnModule<ModuleDaprClientGuide>().Register();
     }
+}
+
+public static class ModuleDataChannelBuilderExtensions
+{
+    public static ModuleDataChannelGuide ConfigModuleDataChannel(this WebApplicationBuilder builder,
+        Action<ModuleDataChannelOption>? action = null)
+    {
+        return new ModuleDataChannelGuide().Register(action);
+    }
+}
+
+
+public class ModuleDataChannelGuide : MoModuleGuide<ModuleDataChannel, ModuleDataChannelOption, ModuleDataChannelGuide>
+{
+
+    protected override string[] GetRequestedConfigMethodKeys()
+    {
+        return [nameof(SetChannelBuilder)];
+    }
+    public ModuleDataChannelGuide SetChannelBuilder<TBuilderEntrance>()
+    {
+        ConfigureServices(context =>
+        {
+            context.Services.AddSingleton(typeof(ISetupPipeline), typeof(TBuilderEntrance));
+        });
+        return this;
+    }
+}
+
+/// <summary>
+/// 数据通道配置类
+/// 用于配置数据通道的全局设置和选项
+/// 实现了IMoModuleOptions接口，支持模块化配置
+/// </summary>
+public class ModuleDataChannelOption : MoModuleControllerOption<ModuleDataChannel>
+{
+    /// <summary>
+    /// 最近异常保留数量
+    /// </summary>
+    public int RecentExceptionToKeep { get; set; } = 10;
+
+    /// <summary>
+    /// 初始化线程数
+    /// </summary>
+    public int InitThreadCount { get; set; } = 10;
+
 }
