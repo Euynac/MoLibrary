dcb34dfaf7b01d6ef5f832da2a9bc90f65d516a4
Momean <18819458351@163.com>
2025-07-21 17:19:54 +0800

    优化注册中心模块依赖

--- MoLibrary.RegisterCentre/Modules/RegisterCentre.cs
+++ MoLibrary.RegisterCentre/Modules/RegisterCentre.cs
@@ -5,12 +5,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using MoLibrary.Core.Extensions;
 using MoLibrary.Core.Module;
+using MoLibrary.Core.Module.Interfaces;
 using MoLibrary.Core.Module.Models;
+using MoLibrary.Dapr.Modules;
 using MoLibrary.Tool.MoResponse;
 
 namespace MoLibrary.RegisterCentre.Modules;
 
-public class ModuleRegisterCentre(ModuleRegisterCentreOption option) : MoModule<ModuleRegisterCentre, ModuleRegisterCentreOption, ModuleRegisterCentreGuide>(option)
+public class ModuleRegisterCentre(ModuleRegisterCentreOption option) : MoModuleWithDependencies<ModuleRegisterCentre, ModuleRegisterCentreOption, ModuleRegisterCentreGuide>(option)
 {
     public override EMoModules CurModuleEnum()
     {
@@ -96,4 +98,94 @@ public class ModuleRegisterCentre(ModuleRegisterCentreOption option) : MoModule<
             });
         }
     }
+
+    public override void ClaimDependencies()
+    {
+        DependsOnModule<ModuleDaprClientGuide>().Register();
+    }
+}
+
+public class ModuleRegisterCentreGuide : MoModuleGuide<ModuleRegisterCentre, ModuleRegisterCentreOption, ModuleRegisterCentreGuide>
+{
+    public const string SET_CENTRE_TYPE = nameof(SET_CENTRE_TYPE);
+    protected override string[] GetRequestedConfigMethodKeys()
+    {
+        return [SET_CENTRE_TYPE];
+    }
+
+    /// <summary>
+    /// 注册MoRegisterCentreClient
+    /// </summary>
+    /// <returns></returns>
+    public ModuleRegisterCentreGuide SetAsCentreServer()
+    {
+        ConfigureModuleOption(o =>
+        {
+            o.ThisIsCentreServer = true;
+        }, key: SET_CENTRE_TYPE);
+
+        ConfigureServices(context =>
+        {
+            context.Services.AddSingleton<IRegisterCentreServer, MemoryProviderForRegisterCentre>();
+            context.Services.AddSingleton<IRegisterCentreClientConnector, DaprHttpForConnectClient>();
+        }, key: SET_CENTRE_TYPE);
+
+        return this;
+    }
+    /// <summary>
+    /// 注册MoRegisterCentreClient
+    /// </summary>
+    /// <returns></returns>
+    public ModuleRegisterCentreGuide SetAsCentreClient<TServer, TClient>()
+        where TServer : class, IRegisterCentreServerConnector where TClient : class, IRegisterCentreClient
+    {
+        ConfigureModuleOption(o =>
+        {
+            o.ThisIsCentreClient = true;
+        }, key: SET_CENTRE_TYPE);
+        ConfigureServices(context =>
+        {
+            context.Services.AddSingleton<IRegisterCentreServerConnector, TServer>();
+            context.Services.AddSingleton<IRegisterCentreClient, TClient>();
+        }, key: SET_CENTRE_TYPE);
+        return this;
+    }
+}
+public static class ModuleRegisterCentreBuilderExtensions
+{
+    public static ModuleRegisterCentreGuide ConfigModuleRegisterCentre(this WebApplicationBuilder builder, Action<ModuleRegisterCentreOption>? action = null)
+    {
+        return new ModuleRegisterCentreGuide().Register(action);
+    }
+}
+
+public class ModuleRegisterCentreOption : MoModuleControllerOption<ModuleRegisterCentre>
+{
+    /// <summary>
+    /// 设定当前微服务是注册中心
+    /// </summary>
+    internal bool ThisIsCentreServer { get; set; } = false;
+    /// <summary>
+    /// 设定当前微服务是注册中心客户端
+    /// </summary>
+    internal bool ThisIsCentreClient { get; set; } = false;
+
+    /// <summary>
+    /// TODO 最大并发执行数量
+    /// </summary>
+    public int MaxParallelInvokerCount { get; set; }
+
+    /// <summary>
+    /// 客户端心跳频率
+    /// </summary>
+    public int HeartbeatDuration { get; set; } = 10;
+
+    /// <summary>
+    /// 客户端注册中心重试次数
+    /// </summary>
+    public int ClientRetryTimes { get; set; } = 3;
+    /// <summary>
+    /// 客户端重试频率
+    /// </summary>
+    public int RetryDuration { get; set; } = 5;
 }
