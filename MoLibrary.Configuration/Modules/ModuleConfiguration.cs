b299ac5fbbdbb9dc4d6f9736c87da3613c937579
Momean <34342521+Tairitsua@users.noreply.github.com>
2025-07-29 16:50:40 +0800

    修复路由问题：由于调整Configuration依赖Controller导致Controller模块提前初始化，导致后续执行的路由未能成功注册

--- MoLibrary.Configuration/Modules/ModuleConfiguration.cs
+++ MoLibrary.Configuration/Modules/ModuleConfiguration.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using MoLibrary.Configuration.Annotations;
-using MoLibrary.Configuration.Controllers;
 using MoLibrary.Configuration.Interfaces;
 using MoLibrary.Configuration.Model;
 using MoLibrary.Configuration.Providers;
@@ -23,7 +22,7 @@ using MoLibrary.Tool.MoResponse;
 
 namespace MoLibrary.Configuration.Modules;
 
-public class ModuleConfiguration(ModuleConfigurationOption option) : MoModuleWithDependencies<ModuleConfiguration, ModuleConfigurationOption, ModuleConfigurationGuide>(option), IWantIterateBusinessTypes
+public class ModuleConfiguration(ModuleConfigurationOption option) : MoModule<ModuleConfiguration, ModuleConfigurationOption, ModuleConfigurationGuide>(option), IWantIterateBusinessTypes
 {
     private IServiceCollection _services = null!;
     private MethodInfo _method = null!;
@@ -70,14 +69,55 @@ public class ModuleConfiguration(ModuleConfigurationOption option) : MoModuleWit
 
         _method = method;
     }
-
-    public override void ClaimDependencies()
+    public override void ConfigureEndpoints(IApplicationBuilder app)
     {
-        // 注册Controller依赖
-        DependsOnModule<ModuleControllersGuide>().Register()
-            .RegisterMoControllers<ModuleConfigurationController>(Option);
-    }
+        app.UseEndpoints(endpoints =>
+        {
+            var tagGroup = new List<OpenApiTag> { new() { Name = option.GetApiGroupName(), Description = "热配置相关内置接口" } };
+            endpoints.MapGet(MoConfigurationConventions.GetConfigStatus, async (HttpResponse response,
+                HttpContext context, [FromQuery] bool? onlyCurDomain,
+                [FromServices] IMoConfigurationCardManager manager) =>
+            {
+                return Res.Create(manager.GetDomainConfigs(onlyCurDomain), ResponseCode.Ok).GetResponse();
+            }).WithName("获取热配置状态信息").WithOpenApi(operation =>
+            {
+                operation.Summary = "获取热配置状态信息";
+                operation.Description = "获取热配置状态信息";
+                operation.Tags = tagGroup;
+                return operation;
+            });
 
+            endpoints.MapGet("/option/debug", async (HttpResponse response, HttpContext context) =>
+            {
+                var res = new
+                {
+                    debug = MoConfigurationManager.GetDebugView().Split(Environment.NewLine),
+                };
+
+                return res;
+            }).WithName("获取DebuggingView")
+                .WithOpenApi(operation =>
+                {
+                    operation.Summary = "获取DebuggingView";
+                    operation.Description = "展示配置项来源数据以及提供者";
+                    operation.Tags = tagGroup;
+                    return operation;
+                });
+
+            endpoints.MapGet("/option/providers", async (HttpResponse response, HttpContext context) =>
+            {
+                var res = MoConfigurationManager.GetProviders();
+                return res;
+            }).WithName("获取配置提供者")
+                .WithOpenApi(operation =>
+                {
+                    operation.Summary = "获取配置提供者";
+                    operation.Description = "获取配置提供者";
+                    operation.Tags = tagGroup;
+                    return operation;
+                });
+        });
+    }
     public override void PostConfigureServices(IServiceCollection services)
     {
         //巨坑：当Option的属性是List或Array等类型，有多个Configuration来源，那么这里面的元素会Append而不是替换。设计如此。dotnet/runtime #36384
