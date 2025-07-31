f6e936ba1f46322173ebf9055578c57d93a986bf
Momean <34342521+Euynac@users.noreply.github.com>
2025-07-31 15:07:03 +0800

    Add WherePartition method to ListExtensions class
    Introduces a new method `WherePartition<T>` that partitions a list into matched and unmatched elements based on a predicate. The method iterates through the source list only once and returns a tuple containing both lists. XML documentation comments have been added for clarity, and the `System.Linq` namespace is now included.

--- MoLibrary.Tool/Extensions/ListExtensions.cs
+++ MoLibrary.Tool/Extensions/ListExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MoLibrary.Tool.Extensions;
 
@@ -30,5 +31,28 @@ public static class ListExtensions
         {
             yield return list.GetRange(i, Math.Min(chunkSize, list.Count - i));
         }
+    }
+
+    /// <summary>
+    /// 根据指定条件将列表分为满足条件和不满足条件的两个列表，只遍历一次
+    /// </summary>
+    /// <typeparam name="T">列表元素类型</typeparam>
+    /// <param name="source">源列表</param>
+    /// <param name="predicate">筛选条件</param>
+    /// <returns>元组，第一个列表为满足条件的元素，第二个列表为不满足条件的元素</returns>
+    public static (List<T> matched, List<T> unmatched) WherePartition<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+    {
+        var matched = new List<T>();
+        var unmatched = new List<T>();
+
+        foreach (var item in source)
+        {
+            if (predicate(item))
+                matched.Add(item);
+            else
+                unmatched.Add(item);
+        }
+
+        return (matched, unmatched);
     }
 }
