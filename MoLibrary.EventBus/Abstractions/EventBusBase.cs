301cc8746d4a279573f7735d256a5d1c930135e2
Momean <34342521+Tairitsua@users.noreply.github.com>
2025-12-01 09:28:03 +0800

    feat: add SynchronizationContextRemover utility class
    - Create new SynchronizationContextRemover struct in MoLibrary.Tool.Utils
    - Extract SynchronizationContextRemover from EventBusBase to
      reusable utility
    - Implements INotifyCompletion to remove synchronization context
      during async operations
    - Allows async operations to run on thread pool instead of being
      marshalled back to original context
    - Remove unused System.Runtime.CompilerServices import from
      EventBusBase

--- MoLibrary.EventBus/Abstractions/EventBusBase.cs
+++ MoLibrary.EventBus/Abstractions/EventBusBase.cs
@@ -1,7 +1,6 @@
 using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.DependencyInjection;
 using MoLibrary.Core.Extensions;
 using MoLibrary.EventBus.Attributes;
@@ -315,35 +314,5 @@ public abstract class EventBusBase : IMoEventBus
         public Type EventType { get; } = eventType;
 
         public List<IEventHandlerFactory> EventHandlerFactories { get; } = eventHandlerFactories;
-    }
-
-    // Reference from
-    // https://blogs.msdn.microsoft.com/benwilli/2017/02/09/an-alternative-to-configureawaitfalse-everywhere/
-    protected struct SynchronizationContextRemover : INotifyCompletion
-    {
-        public bool IsCompleted => SynchronizationContext.Current == null;
-
-        public void OnCompleted(Action continuation)
-        {
-            var prevContext = SynchronizationContext.Current;
-            try
-            {
-                SynchronizationContext.SetSynchronizationContext(null);
-                continuation();
-            }
-            finally
-            {
-                SynchronizationContext.SetSynchronizationContext(prevContext);
-            }
-        }
-
-        public SynchronizationContextRemover GetAwaiter()
-        {
-            return this;
-        }
-
-        public void GetResult()
-        {
-        }
     }
 }
---
+++ MoLibrary.Tool/Utils/SynchronizationContextRemover.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace MoLibrary.Tool.Utils;
+
+/// <summary>
+/// An alternative to ConfigureAwait(false) everywhere.
+/// Removes the current synchronization context when awaited, allowing subsequent async operations
+/// to run on thread pool threads instead of being marshalled back to the original context.
+/// </summary>
+/// <remarks>
+/// Reference from: https://blogs.msdn.microsoft.com/benwilli/2017/02/09/an-alternative-to-configureawaitfalse-everywhere/
+/// </remarks>
+public struct SynchronizationContextRemover : INotifyCompletion
+{
+    public bool IsCompleted => SynchronizationContext.Current == null;
+
+    public void OnCompleted(Action continuation)
+    {
+        var prevContext = SynchronizationContext.Current;
+        try
+        {
+            SynchronizationContext.SetSynchronizationContext(null);
+            continuation();
+        }
+        finally
+        {
+            SynchronizationContext.SetSynchronizationContext(prevContext);
+        }
+    }
+
+    public SynchronizationContextRemover GetAwaiter()
+    {
+        return this;
+    }
+
+    public void GetResult()
+    {
+    }
+}
